6c0316ad757ad5601dedc6f238274f4027a6eb5d
Robininho <robin.halberda@orange.fr>
2018-10-11 11:18:43 +0200

    FInish

--- js.js
+++ js.js
@@ -1,9 +1,9 @@
 // onmouseover="image1.src='images/image1_2.jpg'" onmouseout="image1.src='images/image1.jpg'"
 
-image1.addEventListener("mouseover",function(){
+image1.addEventListener('mouseover', function(){
   image1.src='images/image1_2.jpg';
 });
 
-image1.addEventListener("mouseout", function(){
+image1.addEventListener('mouseout', function(){
   image1.src='images/image1.jpg';
 });
